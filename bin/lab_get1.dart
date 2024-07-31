1448cc3c21b7db4400496f44b1c71395cd15047d
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-07-31 10:50:24 +0300

    project

---
+++ bin/lab_get1.dart
@@ -0,0 +1,5 @@
+import 'package:lab_get1/lab_get1.dart' as lab_get1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lab_get1.calculate()}!');
+}
---
+++ test/lab_get1_test.dart
@@ -0,0 +1,8 @@
+import 'package:lab_get1/lab_get1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
